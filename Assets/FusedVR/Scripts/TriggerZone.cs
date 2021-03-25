0f35929c775154269e49469daf8e72b6c96f10cf
Vasanth Mohan <vmohan7@stanford.edu>
2021-03-24 18:56:03 -0700

    VR Metamask

---
+++ Assets/FusedVR/Scripts/TriggerZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+       if ( other.name.Contains("Token") ) {
+            count++;
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name.Contains("Token")) {
+            count--;
+        }
+    }
+
+    public int GetCount() {
+        return count;
+    }
+}
