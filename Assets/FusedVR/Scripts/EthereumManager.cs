a1a1ebf90c113d41f3928d11c97143f0396a9bf8
Fuseman <vmohan2211@gmail.com>
2021-04-06 21:24:03 -0700

    WebXR & Metamask (#1)
    Add WebXR

---
+++ Assets/FusedVR/Scripts/EthereumManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using Nethereum.Web3;
+using Nethereum.JsonRpc.UnityClient;
+using UnityEngine;
+using Nethereum.RPC.Eth.DTOs;
+using TMPro;
+
+public class EthereumManager : MonoBehaviour {
+
+    public string Url = "https://ropsten.infura.io/v3/7aab67d98b0b42f181905b62c2025d9a";
+    public string AddressTo = "0x7336bb564f9007A1f0A06985255B0Ce0F44BDB3D";
+
+    public TextMeshPro mText;
+
+    public delegate void OnTransactionProcessed(string txHash);
+    public OnTransactionProcessed onTransactionDelegate;
+
+    [DllImport("__Internal")]
+    private static extern string GetAccount();
+
+    [DllImport("__Internal")]
+    private static extern void SendTransaction(string to, string data, string returnObj, string returnFunc);
+
+    public void TransferRequest(float amount) {
+        Logger("Sending " + amount);
+        SendTransaction(AddressTo, amount.ToString(), gameObject.name, "TransactionCallback");
+    }
+
+    public string GetAddress() {
+        return GetAccount();
+    }
+
+    public IEnumerator GetAccountBalance(string account , System.Action<float> callback) {
+        var balanceRequest = new EthGetBalanceUnityRequest(Url);
+        yield return balanceRequest.SendRequest(account, BlockParameter.CreateLatest());
+        var ether = Web3.Convert.FromWei(balanceRequest.Result.Value);
+        if (callback != null) callback( decimal.ToSingle(ether) );
+
+        Logger("Balance is " + ether);
+    }
+
+    public void TransactionCallback(string txHash) {
+        Logger("Got Tx Hash from Unity: " + txHash);
+        StartCoroutine(TransactionPolling(txHash));
+    }
+
+    private IEnumerator TransactionPolling(string txHash) {
+        //create a poll to get the receipt when mined
+        var transactionReceiptPolling = new TransactionReceiptPollingRequest(Url);
+        //checking every 2 seconds for the receipt
+        yield return transactionReceiptPolling.PollForReceipt(txHash, 2);
+
+        Logger ( "Transaction mined" );
+
+        onTransactionDelegate?.Invoke(txHash);
+    }
+
+    private void Logger(string log) {
+        mText.text = log;
+        Debug.Log(log);
+    }
+}
---
+++ Assets/FusedVR/Scripts/TokenManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tilia.Interactions.Interactables.Interactables;
+
+public class TokenManager : MonoBehaviour
+{
+    public EthereumManager ether;
+    public InteractableFacade tokenPrefab;
+
+    public Transform spawnLocation;
+
+    public const float TokenValue = 0.1f;
+
+    public TriggerZone zone;
+
+    private List<GameObject> tokens = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(ether.GetAccountBalance(GetAddress(), BalanceCallback));
+        ether.onTransactionDelegate += TransactionProcessed;
+    }
+
+    void BalanceCallback(float balance) {
+        Debug.LogError(balance);
+        int numTokens = Mathf.FloorToInt(balance / TokenValue);
+        StartCoroutine( CreateTokens(numTokens) );
+    }
+
+    IEnumerator CreateTokens(int numTokens) {
+        for (int i = 0; i < numTokens; i++) {
+            tokens.Add( Instantiate(tokenPrefab, spawnLocation.position, spawnLocation.rotation).gameObject );
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    public void SendTransction() {
+        int count = zone.GetCount();
+        Debug.Log(count);
+
+        ether.TransferRequest(count * TokenValue);
+    }
+
+    void TransactionProcessed(string txHash) {
+        for (int i = 0; i < tokens.Count; i++) {
+            Destroy(tokens[i]);
+        }
+        tokens.Clear();
+
+        StartCoroutine(ether.GetAccountBalance(GetAddress(), BalanceCallback));
+    }
+
+    private string GetAddress() {
+        string address = "0x74BAA21278E661eCea04992d5e8fBE6c29cF6f64"; //test address
+#if !UNITY_EDITOR
+        address = ether.GetAddress();
+#endif
+
+        return address;
+    }
+}
